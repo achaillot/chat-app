855a3ccce9595da32b87a45d4e35e0a54795a76a
Aurelien Chaillot <chaillot.aurelien@gmail.com>
2021-03-09 11:26:23 +0100

    Create Contact Component

---
+++ src/components/Contact.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Contact.css';
+
+function Contact() {
+    return (
+        <div className='Contact'>
+            <img class="avatar" src="https://randomuser.me/api/portraits/women/16.jpg"></img>
+            <div>
+                <h3 class="name">Leah Jensen</h3>
+                <div class="status status-online">
+                    <div class="status-online"></div>
+                    <p class="status-text">online</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Contact;
