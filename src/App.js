56176e64f0991a8ff95510a5a2a3e7887d4edac0
Aurelien Chaillot <chaillot.aurelien@gmail.com>
2021-03-14 18:59:53 +0100

    Quête React 03

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import Contact from './components/Contact'
+import ContactList from './components/ContactList';
 
 function App() {
   return (
     <div className="App">
-      <Contact 
-        name = 'Arianna Lowe'
-        avatar = 'https://randomuser.me/api/portraits/women/18.jpg'
-        online
-      />
-      <Contact 
-        name = 'Paul Taylor'
-        avatar = 'https://randomuser.me/api/portraits/men/36.jpg'
-        online
-      />
-      <Contact 
-        name = 'Brennan Lucas'
-        avatar = 'https://randomuser.me/api/portraits/men/18.jpg'
-      />
+      <ContactList />
     </div>
   );
 }
